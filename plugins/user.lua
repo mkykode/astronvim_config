6702dbf65dcee2350ab740eae5ee3022d4497e45
Jull Weber <jull@monkeykodeagency.com>
2023-05-08 10:44:34 -0700

    feat: options for neo tree

--- plugins/user.lua
+++ plugins/user.lua
@@ -52,4 +52,14 @@ return {
       }
     end,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
 }
